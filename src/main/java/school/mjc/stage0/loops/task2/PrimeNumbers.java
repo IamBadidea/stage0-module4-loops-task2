e7fb1f034caa7712094168d39123c0eabeed309a
Filipp <okruzhko.f@gmail.com>
2023-10-11 00:01:30 +0400

    Fix PrimeNumbers #2 in s0m4t2

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,25 +1,36 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
-    public void printPrimeNumbers(int printToInclusive) {
-        System.out.println(2);
-        System.out.println(3);
-        int i = 4;
-        boolean flag = false;
-        while (i < printToInclusive) {
-            int j = i - 1;
-            while (j >= 1) {
-                if (i % j == 0) {
-                    flag = true;
-                }
-                j--;
-            }
 
-            if (flag == false) {
+    public static void main(String[] args) {
+        PrimeNumbers pn = new PrimeNumbers();
+        pn.printPrimeNumbers(100);
+    }
+    public void printPrimeNumbers(int printToInclusive) {
+
+        int i = 2;
+        while (printToInclusive >= i) {
+            if (isPrime(i)) {
                 System.out.println(i);
             }
-
             i++;
         }
+    }
+
+    private boolean isPrime(int number) {
+        if (number == 2 || number == 3) {
+            return true;
+        }
+
+        int i = number - 1;
+
+        while (i > 1) {
+            if (number % i == 0) {
+                return false;
+            }
+            i--;
+        }
+
+        return true;
     }
 }
