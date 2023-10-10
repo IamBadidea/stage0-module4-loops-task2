9097c5c80e0d3d37865c48ce155c9f6c38102137
Filipp <okruzhko.f@gmail.com>
2023-10-11 01:31:36 +0400

    Fix Multiplication in s0m4t2

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -1,15 +1,45 @@
 package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
+
+    public static void main(String[] args) {
+        Multiplication m = new Multiplication();
+        m.printMultiplied(-4);
+    }
     public void printMultiplied(int multiplyByAndToInclusive) {
+        if (multiplyByAndToInclusive >= 0) {
+            fromZero(multiplyByAndToInclusive);
+        } else {
+            fromNegativeNumber(multiplyByAndToInclusive);
+        }
+    }
+
+    private void fromZero(int toPositiveNumber) {
         int i = 1;
         System.out.println(0);
-        while (i <= multiplyByAndToInclusive) {
+        while (i <= toPositiveNumber) {
             int multiply = 1;
-            while (i <= multiplyByAndToInclusive) {
-                multiply *= i;
+            int j = i;
+            while (j >= 1) {
+                multiply *= j;
+                j--;
             }
             System.out.println(multiply);
+            i++;
+        }
+    }
+    private void fromNegativeNumber(int toNegativeNumber) {
+        int i = -1;
+        System.out.println(0);
+        while (i >= toNegativeNumber) {
+            int multiply = 1;
+            int j = i;
+            while (j <= -1) {
+                multiply *= j;
+                j++;
+            }
+            System.out.println(multiply);
+            i--;
         }
     }
 }
