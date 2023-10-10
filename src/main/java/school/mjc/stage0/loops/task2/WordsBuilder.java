e08cbf7db221b0b6978c5208e5cda0d64d025615
Filipp <okruzhko.f@gmail.com>
2023-10-11 00:25:12 +0400

    Fix WordsBuilder in s0m4t2

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,11 +1,16 @@
 package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
+    public static void main(String[] args) {
+        WordsBuilder wb = new WordsBuilder();
+        wb.buildPhrase('s', 'e', 'x');
+    }
     public void buildPhrase(char... chars) {
         int i = 0;
 
         while (i < chars.length) {
             System.out.print(chars[i]);
+            i++;
         }
     }
 }
